ada6106623cfb4212869758aed7ea095fe841e18
xleine <xleine@qq.com>
2025-10-31 15:20:03 +0800

    feat: 添加 Yao 定义

---
+++ src/gua/yao.rs
@@ -0,0 +1,32 @@
+/**
+* 爻，卦的基础单位
+* 分为阴和阳
+*/
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct Yao {
+    pub status: bool,
+}
+
+impl Yao {
+    pub const YIN: Yao = Yao { status: false };
+    pub const YANG: Yao = Yao { status: true };
+
+    pub fn new(is_yang: bool) -> Self {
+        Yao { status: is_yang }
+    }
+
+    pub fn is_yang(&self) -> bool {
+        self.status
+    }
+
+    pub fn is_yin(&self) -> bool {
+        !self.status
+    }
+
+    /**
+     * 翻转
+     */
+    pub fn reverse(&mut self) {
+        self.status = !self.status;
+    }
+}
