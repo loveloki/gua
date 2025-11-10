d7227878f3bd32c1d401458decd304b249866329
xleine <xleine@qq.com>
2025-11-11 00:28:56 +0800

    feat: 简化 run.sh 脚本

--- run.sh
+++ run.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-output=$(dist build 2>&1 | tee /dev/stderr)
-if [ $? -eq 0 ]; then
-  bin_path=$(echo "$output" | grep "\[bin\] gua" -B1 | head -1 | sed 's/.* //')
-  dir_name=$(basename "$bin_path" .tar.xz)
-  ./target/distrib/$dir_name/gua
-fi
+
+dist build && ./target/distrib/*/gua
