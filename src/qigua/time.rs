99e8e11294117e214993f685a86c9eb8f20e7066
xleine <xleine@qq.com>
2025-11-17 19:19:20 +0800

    wippppp

--- src/qigua/time.rs
+++ src/qigua/time.rs
@@ -1,3 +1,5 @@
+use chinese_lunisolar_calendar::{LunisolarDate, SolarDate};
+use chrono::{Datelike, Local, NaiveDate};
 use gpui::{
     App, AppContext, Context, Entity, IntoElement, ParentElement, Render, SharedString, Styled,
     Window, div,
@@ -5,6 +7,7 @@ use gpui::{
 use gpui_component::{
     Disableable, StyledExt,
     button::{Button, ButtonVariants},
+    date_picker::{DatePicker, DatePickerEvent, DatePickerState},
 };
 
 use crate::qigua::core::QiGuaCore;
@@ -39,7 +42,10 @@ impl Render for Time {
 /**
  * 输入时间来计算卦象
  */
-pub struct TimeContent {}
+pub struct TimeContent {
+    date_picker: Entity<DatePickerState>,
+    date_picker_value: Option<NaiveDate>,
+}
 
 impl TimeContent {
     pub fn view(window: &mut Window, cx: &mut App) -> Entity<Self> {
@@ -47,34 +53,68 @@ impl TimeContent {
     }
 
     fn new(window: &mut Window, cx: &mut Context<Self>) -> Self {
-        Self {}
+        let date_picker = cx.new(|cx| {
+            let mut picker = DatePickerState::new(window, cx).number_of_months(1);
+            picker.set_date(Local::now().naive_local().date(), window, cx);
+
+            picker
+        });
+        let _subscriptions = vec![cx.subscribe(&date_picker, |this, _, ev, _| match ev {
+            DatePickerEvent::Change(date) => {
+                this.date_picker_value = date.start();
+            }
+        })];
+
+        Self {
+            date_picker,
+            date_picker_value: None,
+        }
+    }
+
+    fn data_picker_content(&self) -> impl IntoElement {
+        DatePicker::new(&self.date_picker)
     }
 }
 
 impl Render for TimeContent {
     fn render(&mut self, _: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
         let entity = cx.entity();
-        div()
+
+        let mut content = div()
             .w_80()
             .p_2()
             .v_flex()
             .gap_2()
             .child(NAME)
-            .child("暂未实现！")
-            .child(
-                Button::new("calc")
-                    .label("开始计算")
-                    .primary()
-                    .disabled(true)
-                    .on_click(move |_, _, cx| {
-                        cx.update_entity(
-                            &entity,
-                            |input: &mut TimeContent, context: &mut Context<TimeContent>| {
-                                input.calc_gua(context)
-                            },
-                        );
-                    }),
-            )
+            .child(self.data_picker_content());
+
+        if let Some(select_date) = self.date_picker_value {
+            let year = select_date.year();
+            let month = select_date.month();
+            let day = select_date.day();
+
+            let lunisolar_date =
+                LunisolarDate::from_solar_date(SolarDate::from_ymd(year, month, day).unwrap()).unwrap();
+
+            content = content
+                .child(format!("公历: {}-{}-{}", year, month, day))
+                .child(format!("农历: {}", lunisolar_date));
+        }
+
+        content.child(
+            Button::new("calc")
+                .label("开始计算")
+                .primary()
+                .disabled(self.date_picker_value.is_none())
+                .on_click(move |_, _, cx| {
+                    cx.update_entity(
+                        &entity,
+                        |input: &mut TimeContent, context: &mut Context<TimeContent>| {
+                            input.calc_gua(context)
+                        },
+                    );
+                }),
+        )
     }
 }
 
